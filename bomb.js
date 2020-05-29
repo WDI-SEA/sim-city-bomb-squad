1fba12173b6d87eb34755bd8f5b76ce2065255b7
009kings <009kings@gmail.com>
2020-05-29 15:22:49 -0700

    Stubbed out some functions

--- bomb.js
+++ bomb.js
@@ -1,1 +1,58 @@
-console.log('loaded!');
+document.addEventListener('DOMContentLoaded', function() {
+  /* --------- DOM REFS ---------*/
+  let body = document.querySelector('body');
+  let wireBox = document.getElementById('wirebox');
+  let resetBtn = document.getElementById('reset');
+  let timer = document.getElementById('timer');
+
+  /* --------- Game Logic Variables ------- */
+  const STARTING_TIME = 30;
+  let remainingTime = 0;
+  let gameOver = false;
+  let countdown = null; // will hold my countdown interval
+
+  let wiresToCut = [];
+
+  let wireState = {
+    blue: false,
+    green: false,
+    red: false,
+    white: false,
+    yellow: false
+  }
+
+  /* ----- Event Listeners ------ */
+  resetBtn.addEventListener('click', reset);
+  wireBox.addEventListener('click', wireClick);
+
+  function reset() {
+    console.log("clicked reset");
+    init()
+  }
+
+  function init() {
+    remainingTime = STARTING_TIME;
+
+    // set wires to cut
+    for (const color in wireState) {
+      let randoNum = Math.random();
+      if (randoNum > 0.5) {
+        wiresToCut.push(color)
+      }
+    }
+    // FOR DEBUGGING
+    console.log(wiresToCut)
+    countdown = setInterval(updateClock, 1000)
+    resetBtn.disabled = true;
+  }
+
+  function wireClick(e) {
+    console.log("clicked wire box")
+    console.log(e.target)
+  }
+
+  function updateClock() {
+    console.log('clock updating')
+  }
+  
+})
